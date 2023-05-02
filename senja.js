f0949966299182a163fdc1bd3e74218a16ff2344
abdul mukti <andulmukti40390@gmail.com>
2023-05-02 21:15:35 +0700

    add some fiturs

--- senja.js
+++ senja.js
@@ -1,17 +1,51 @@
 const navbar = document.querySelector(".navbar-menu");
+const humberger = document.querySelector("#menu")
+const searchForm = document.querySelector(".search-form")
+const searcBox = document.querySelector("#pencarian")
+const searchButton = document.querySelector("#search")
+const shopingCart = document.querySelector(".shoping-cart")
+const cartButton = document.querySelector("#sopping")
+const detail = document.querySelectorAll("#eye")
+const modal = document.querySelector(".modal")
 
+// hamburger
 document.querySelector("#menu").onclick = () => {
     navbar.classList.toggle("active")
 }
-
-const humberger = document.querySelector("#menu")
+// shoping-cart
+document.querySelector("#sopping").onclick = (e) => {
+    shopingCart.classList.toggle("active")
+    e.preventDefault()
+}
+// search
+document.querySelector("#search").onclick = (e) => {
+    searchForm.classList.toggle("active")
+    searcBox.focus()
+    e.preventDefault()
+}
 // click selain navbar //
 document.addEventListener("click",function(e){
     if(!navbar.contains(e.target) && !humberger.contains(e.target)){
         navbar.classList.remove("active")
     }
-}) 
-const h1 = document.querySelector(".h1")
+    if(!searchForm.contains(e.target) && !searchButton.contains(e.target)){
+        searchForm.classList.remove("active")
+    }
+    if(!shopingCart.contains(e.target) && !cartButton.contains(e.target)){
+        shopingCart.classList.remove("active")
+    }
+})
+// detail
+detail.forEach((btn) => {
+    btn.onclick = (e) => {
+        modal.style.display = "flex";
+        e.preventDefault()
+    }
+})
+document.querySelector(".close-icon").onclick = (e) => {
+    modal.style.display = "none";
+    e.preventDefault()
+}
 
 
 // window.addEventListener("scroll",(e) => {
